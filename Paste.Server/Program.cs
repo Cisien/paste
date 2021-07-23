f9f1ae22eb11427120db53285099aed2ce97cb0a
Cisien <ccurwick@exoronet.net>
2021-07-22 17:58:24 -0700

    Make logging not terrible.

--- Paste.Server/Program.cs
+++ Paste.Server/Program.cs
@@ -1,22 +1,44 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System.Threading.Tasks;
+
 
 namespace Paste.Server
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseConfiguration(new ConfigurationBuilder()
-                    .AddCommandLine(args)
-                    .Build())
-                .UseStartup<Startup>()
-                .Build();
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureLogging((context, logging) =>
+                {
+                    logging.AddSimpleConsole(o =>
+                    {
+                        o.SingleLine = context.HostingEnvironment.IsProduction();
+                        o.ColorBehavior = context.HostingEnvironment.IsProduction() ? LoggerColorBehavior.Disabled : LoggerColorBehavior.Enabled;
+                        o.UseUtcTimestamp = context.HostingEnvironment.IsProduction();
+                        o.TimestampFormat = "o";
+                    });
+
+                    if (context.HostingEnvironment.IsDevelopment())
+                    {
+                        logging.AddDebug();
+                    }
+                })
+                .UseStartup<Startup>();
+            });
+
+            return host;
+        }
     }
 }
+
